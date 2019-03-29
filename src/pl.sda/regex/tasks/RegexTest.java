4f689d2f5fb274532625d9fd77d34d668cac8808
mrzepinski <Maciej.Rzepinski@wolterskluwer.com>
2019-03-29 20:36:45 +0100

    Better regex examples

--- src/pl.sda/regex/tasks/RegexTest.java
+++ src/pl.sda/regex/tasks/RegexTest.java
@@ -2,29 +2,27 @@ package pl.sda.regex.tasks;
 
 public class RegexTest {
     public static void main(String[] args) {
-//        String s1 = "10-123";
-//        String s2 = "80-432";
-//        String s3 = "1-300";
-//        String s4 = "ab-cde";
-//        String s5 = "80+432";
-//        String s6 = "80-4324";
-//        String s7 = "800-432";
-//        String s8 = "80-43";
+        String s1 = "10-123";
+        String s2 = "80-432";
+        String s3 = "1-300";
+        String s4 = "ab-cde";
+        String s5 = "80+432";
+        String s6 = "80-4324";
+        String s7 = "800-432";
+        String s8 = "80-43";
 
-//        System.out.println("s1 -> " + RegexUtil.isValidPostalCode(s1));
-//        System.out.println("s2 -> " + RegexUtil.isValidPostalCode(s2));
-//        System.out.println("s3 -> " + RegexUtil.isValidPostalCode(s3));
-//        System.out.println("s4 -> " + RegexUtil.isValidPostalCode(s4));
-//        System.out.println("s5 -> " + RegexUtil.isValidPostalCode(s5));
-//        System.out.println("s6 -> " + RegexUtil.isValidPostalCode(s6));
-//        System.out.println("s7 -> " + RegexUtil.isValidPostalCode(s7));
-//        System.out.println("s8 -> " + RegexUtil.isValidPostalCode(s8));
+        System.out.println("s1 -> " + RegexUtil.isValidPostalCode(s1));
+        System.out.println("s2 -> " + RegexUtil.isValidPostalCode(s2));
+        System.out.println("s3 -> " + RegexUtil.isValidPostalCode(s3));
+        System.out.println("s4 -> " + RegexUtil.isValidPostalCode(s4));
+        System.out.println("s5 -> " + RegexUtil.isValidPostalCode(s5));
+        System.out.println("s6 -> " + RegexUtil.isValidPostalCode(s6));
+        System.out.println("s7 -> " + RegexUtil.isValidPostalCode(s7));
+        System.out.println("s8 -> " + RegexUtil.isValidPostalCode(s8));
         System.out.println(RegexUtil.isVaildDateFormat("12.09.1992"));
         System.out.println(RegexUtil.isVaildDateFormat("dfjdaslkdjasdl"));
         System.out.println(RegexUtil.isVaildDateFormat("1.10.200"));
         System.out.println(RegexUtil.isVaildDateFormat("02.15.188"));
         System.out.println(RegexUtil.isVaildDateFormat("45.10.188"));
-
-
    }
 }
--- src/pl.sda/regex/tasks/RegexUtil.java
+++ src/pl.sda/regex/tasks/RegexUtil.java
@@ -1,20 +1,15 @@
 package pl.sda.regex.tasks;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class RegexUtil {
-
     public static boolean isValidPostalCode(String text) {
-        Pattern pattern = Pattern.compile("[\\d]{2}-[\\d]{3}");
-//        Pattern pattern = Pattern.compile("[0-9]{2}-[0-9]{3}");
-        Matcher matcher = pattern.matcher(text);
-        return matcher.matches();
+        return false;
+    }
+
+    public static boolean isValidHttpAddress(String text) {
+        return false;
     }
 
     public static boolean isVaildDateFormat(String text) {
-        Pattern pattern = Pattern.compile("[0-3]?\\d\\.[0-1]\\d\\.[\\d]{1,4}");
-        Matcher matcher = pattern.matcher(text);
-        return matcher.matches();
+        return false;
     }
 }

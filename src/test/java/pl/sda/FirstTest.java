5110be7918955b5ca5de21682e496b18717dc92a
mrzepinski <Maciej.Rzepinski@wolterskluwer.com>
2019-04-05 21:33:03 +0200

    Maven structure

---
+++ src/test/java/pl/sda/FirstTest.java
@@ -0,0 +1,11 @@
+package pl.sda;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    public void shouldTwoEqualTwo() {
+       Assertions.assertEquals(2, 2);
+    }
+}

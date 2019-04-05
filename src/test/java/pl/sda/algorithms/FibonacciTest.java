abd78733035006ffa93c5ecc8d12a6c783dbc839
mrzepinski <Maciej.Rzepinski@wolterskluwer.com>
2019-04-05 21:44:39 +0200

    Fibonacci tests

---
+++ src/test/java/pl/sda/algorithms/FibonacciTest.java
@@ -0,0 +1,39 @@
+package pl.sda.algorithms;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FibonacciTest {
+    @Test
+    public void shouldCreateValidFibonacciSequenceForGivenElements() {
+        // given
+        int n = 4;
+        int[] result = new int[n];
+
+        // when
+        // TODO
+
+        // then
+        // #1
+        Assertions.assertEquals(0, result[0]);
+        Assertions.assertEquals(1, result[1]);
+        Assertions.assertEquals(1, result[2]);
+        Assertions.assertEquals(2, result[3]);
+
+        // #2
+        Assertions.assertArrayEquals(new int[]{0, 1, 1, 2}, result);
+    }
+
+    @Test
+    public void shouldGetProperFibonacciValueForGivenElement() {
+        // given
+        int n = 4;
+
+        // when
+        int result = 0; // TODO
+
+        // then
+        Assertions.assertEquals(2, result);
+        Assertions.assertNotSame(3, result);
+    }
+}

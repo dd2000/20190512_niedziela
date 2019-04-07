1b664792499f52f28e344b2975059cea391afa4d
mrzepinski <Maciej.Rzepinski@wolterskluwer.com>
2019-04-07 10:29:19 +0200

    Recursion examples

--- src/main/java/pl.sda/algorithms/Recursion.java
+++ src/main/java/pl.sda/algorithms/Recursion.java
@@ -2,10 +2,17 @@ package pl.sda.algorithms;
 
 public class Recursion {
     public static void main(String[] args) {
-        System.out.println(sum(1, 10));
+//        System.out.println(sum(1, 10));
+        System.out.println(factor(10));
     }
 
     private static int sum(int start, int end) {
+//          int result = 0;
+//          for (; start <= end; start++) {
+//              result += start;
+//          }
+//          return result;
+
         if (start == end) {
             return start;
         }
@@ -24,5 +31,14 @@ public class Recursion {
 
         // 1 + sum(2, 10) + sum(3, 10) + sum(4, 10) + sum(5, 10) + sum(6, 10) + sum(7, 10) + sum(8, 10) + sum(9, 10) + sum(10, 10)
         // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    }
+
+    private static int factor(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factor(n - 1);
+
+        // 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
     }
 }
